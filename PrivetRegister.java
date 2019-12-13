bc47a4931c57af54592bec061fb6ba48fe0d16c2
gamh86 <46569963+gamh86@users.noreply.github.com>
2019-12-13 21:54:40 +0000

    Create PrivetRegister.java

---
+++ PrivetRegister.java
@@ -0,0 +1,13 @@
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PrivetRegister
+{
+  PrivetRegister() {}
+}
